eeb26c60bb3e2a4952139956e8ed7b8d4754b1fe
Benjamin Martin <ben.martin@activeprospect.com>
2020-04-07 14:16:39 -0500

    trash

--- tf_tests/tf.js
+++ tf_tests/tf.js
@@ -4,6 +4,6 @@
   var tf = document.createElement('script');
   tf.type = 'text/javascript'; tf.async = true;
   tf.src = 'http' + ('https:' == document.location.protocol ? 's' : '') +
-  '://api.trustedform.com/trustedform.js?provide_referrer=' + escape(provideReferrer) + '&field=' + escape(field) + '&l='+new Date().getTime()+Math.random();
+  '://api.trustedform.localhost/trustedform.js?provide_referrer=' + escape(provideReferrer) + '&field=' + escape(field) + '&l='+new Date().getTime()+Math.random();
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(tf, s); }
 )();
